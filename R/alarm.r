470d8f1ab2dc15382bd240cf7c4d0e34e6857ae5
Christian Heckendorf <heckendorfc@gmail.com>
2021-11-29 08:43:18 -0500

    allow a vector of alarms

--- R/alarm.r
+++ R/alarm.r
@@ -1,21 +1,27 @@
 #' alarm
 #' 
-#' A count down until the clock matches the given time.
+#' A count down until the clock matches the given time(s).
 #' 
-#' @param timestr Time that the alarm will go off. 
+#' @param timestr Time(s) that the alarm will go off.
 #' 
 #' @examples
 #' alarm("01:32")
 #' alarm("14:59:15")
+#' alarm(c("01:32","14:59:15"))
 #' 
 #' @export
 alarm = function(timestr)
 {
-  curtime=as.POSIXct(format(Sys.time(), "%Y-%m-%d %H:%M:%S"))
-  dt=difftime(as.POSIXct(paste0(format(Sys.time(),"%Y-%m-%d "),timestr)),curtime,units="secs")
-  if(dt<0){
-    # tomorrow's time (12am?)
-    dt=difftime(as.POSIXct(paste0(format(Sys.time()+24*60*60,"%Y-%m-%d "),timestr)),curtime,units="secs")
+  strtosec=function(ts){
+    curtime=as.POSIXct(format(Sys.time(), "%Y-%m-%d %H:%M:%S"))
+    dt=difftime(as.POSIXct(paste0(format(Sys.time(),"%Y-%m-%d "),ts)),curtime,units="secs")
+    if(dt<0){
+      # tomorrow's time (12am?)
+      dt=difftime(as.POSIXct(paste0(format(Sys.time()+24*60*60,"%Y-%m-%d "),ts)),curtime,units="secs")
+    }
+    as.numeric(dt)
   }
-  timer(seconds=as.numeric(dt), hours=0, minutes=0)
+  for(si in 1:length(timestr)){
+    timer(seconds=strtosec(timestr[si]), hours=0, minutes=0, over_time = si==length(timestr))
+  }
 }
--- R/timer.r
+++ R/timer.r
@@ -4,6 +4,7 @@
 #' 
 #' @param hours,minutes,seconds Time to count down from.
 #' @param recur Should the timer repeat?
+#' @param over_time Display the time elapsed since the alarm?
 #' 
 #' @name timer
 #' @rdname timer
@@ -13,34 +14,34 @@ NULL
 
 #' @rdname timer
 #' @export
-timer = function(hours=0, minutes=0, seconds=0, recur=FALSE)
+timer = function(hours=0, minutes=0, seconds=0, recur=FALSE, over_time=TRUE)
 {
-  timer_clock(hours=hours, minutes=minutes, seconds=seconds, recur=recur)
+  timer_clock(hours=hours, minutes=minutes, seconds=seconds, recur=recur, over_time=over_time)
 }
 
 
 
 #' @rdname timer
 #' @export
-hours = function(hours, recur=FALSE)
+hours = function(hours, recur=FALSE, over_time=TRUE)
 {
-  timer(hours=hours, minutes=0, seconds=0, recur=recur)
+  timer(hours=hours, minutes=0, seconds=0, recur=recur, over_time=over_time)
 }
 
 
 
 #' @rdname timer
 #' @export
-minutes = function(minutes, recur=FALSE)
+minutes = function(minutes, recur=FALSE, over_time=TRUE)
 {
-  timer(minutes=minutes, hours=0, seconds=0, recur=recur)
+  timer(minutes=minutes, hours=0, seconds=0, recur=recur, over_time=over_time)
 }
 
 
 
 #' @rdname timer
 #' @export
-seconds = function(seconds, recur=FALSE)
+seconds = function(seconds, recur=FALSE, over_time=TRUE)
 {
-  timer(seconds=seconds, hours=0, minutes=0, recur=recur)
+  timer(seconds=seconds, hours=0, minutes=0, recur=recur, over_time=over_time)
 }
